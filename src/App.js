85df5e4ebd29fa40f980b35e1b38db937ef108d1
Papon Mateevorakulkij <furby153@gmail.com>
2023-07-31 22:09:16 +0700

    edit to show QR code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +6,38 @@ function App() {
 
   const phoneNumber = '089-441-2049';
   const cleanPhoneNumber = phoneNumber.replace(/-/g, ''); // Remove dashes from the phone number
+  const lineId = '@642mltxi';
 
   const handleCall = () => {
     window.location.href = `tel:${cleanPhoneNumber}`;
   };
 
   const handleCopyLineId = () => {
-    const lineId = '@642mltxi';
     navigator.clipboard.writeText(lineId).then(() => {
-      alert(`Copied Line ID: ${lineId}`);
+      alert(`Copied Line ID: ${lineId}
+
+      You can paste Line ID on search friend in Line Application.
+      คุณสามารถกดวางไลน์ไอดีได้ บนช่องค้นหาเพื่อนในแอพพลิเคชันไลน์`);
     }).catch((err) => {
       console.error('Failed to copy Line ID: ', err);
     });
+  };
+
+  const handleOpenLine = () => {
+
+    // Try opening the Line application using custom URI scheme
+    try {
+      window.location.href = `line://ti/p/~${lineId}`;
+    } catch (error) {
+      // If an error occurs, fallback to copying Line ID to clipboard
+      handleCopyLineId();
+    }
+  };
+
+  const [showQRCode, setShowQRCode] = useState(false);
+
+  const handleShowQRCode = () => {
+    setShowQRCode(true);
   };
 
   return (
@@ -35,9 +55,27 @@ function App() {
       </div>
       <div className='line-section'>
         <h2>Line Official Account</h2>
-        <h3 onClick={handleCopyLineId} style={{ cursor: 'pointer' }}>กดเพิ่มเพื่อน @642mltxi</h3>
+        <h3 onClick={handleOpenLine} style={{ cursor: 'pointer' }}>กดเพิ่มเพื่อน @642mltxi</h3>
         <h3><a href="https://lin.ee/bGqAAAz" target="_blank" rel="noopener noreferrer">https://lin.ee/bGqAAAz</a></h3>
-        <img alt='QR code' src="https://qr-official.line.me/gs/M_642mltxi_BW.png"></img>
+        {showQRCode ? (
+          <img alt='QR code' src="https://qr-official.line.me/gs/M_642mltxi_BW.png"></img>
+        ) : (
+          <button
+          style={{
+            fontSize: '18px',
+            padding: '10px 20px',
+            backgroundColor: '#4CAF50',
+            color: 'white',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            border: 'none',
+            boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.2)',
+          }}
+          onClick={handleShowQRCode}
+          >
+            Show QR Code
+          </button>
+        )}
         <br/>
       </div>
       
